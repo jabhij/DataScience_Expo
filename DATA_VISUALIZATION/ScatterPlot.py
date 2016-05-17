5987d9926a2021d39470e328f2160e7bb788225b
Abhishek Jaiswal <jaisabhii@gmail.com>
2016-05-17 22:11:29 +0530

    ScatterPlot.py

--- DATA_VISUALIZATION/ScatterPlot.py
+++ DATA_VISUALIZATION/ScatterPlot.py
@@ -1,1 +1,15 @@
 
+# Importing matplotlib and elements
+from matplotlib import pyplot as plt
+# Providing Rough data
+Years = [2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016]
+EarthQuakes = [10, 13, 8, 16, 23, 22, 24, 7]
+plt.scatter(Years, EarthQuakes, color='Red')
+# Title
+plt.title('Earthquakes Per Year')
+# Labels
+plt.xlabel('Years')
+plt.ylabel('Occurance / Year')
+# Ticks @ x-axis (Years)
+plt.xticks([i for i in Years], Years)
+plt.show()
